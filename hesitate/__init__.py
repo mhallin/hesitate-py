d6146eab5fd29f03f8bf891b0dada58dd01d5595
mhallin <mhallin@gmail.com>
2015-02-06 22:11:32 +0100

    Initial commit

---
+++ hesitate/__init__.py
@@ -0,0 +1,9 @@
+from .driver import set_initial_probability, \
+    set_target_timing
+from .rewriter import attach_hook
+
+__all__ = [
+    'set_initial_probability', 'set_target_timing',
+
+    'attach_hook',
+]
