a9b7adb7c0aff4cad72940ce1b7af479c237f4f6
mhallin <mhallin@gmail.com>
2015-02-07 15:21:00 +0100

    Silence some linter warnings

--- hesitate/__init__.py
+++ hesitate/__init__.py
@@ -1,12 +1,14 @@
 # Driver must be included first to avoid recursive imports.
 
-from . import driver  # noqa
+from . import driver
 
 from .conf import set_initial_probability, \
     set_target_timing, set_convergence_factor
 from .rewriter import attach_hook
 
 __version__ = '0.0.1'
+
+assert driver  # Dummy assertion to silence linting
 
 __all__ = [
     'set_initial_probability', 'set_target_timing',
